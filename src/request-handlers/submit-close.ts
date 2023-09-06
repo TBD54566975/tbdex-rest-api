84c161ae5855511b763973f329bdb9b920485e87
Moe Jangda <moe@tbd.email>
2023-09-05 22:22:21 -0500

    add initial handling logic for `submitClose`

--- src/request-handlers/submit-close.ts
+++ src/request-handlers/submit-close.ts
@@ -1,5 +1,40 @@
+import type { ErrorDetail, MessageKind } from '@tbd54566975/tbdex'
 import type { SubmitCallback, RequestHandler } from '../types.js'
 
-export function submitClose(_callback: SubmitCallback<'close'>): RequestHandler {
-  return null
+import { Message } from '@tbd54566975/tbdex'
+
+export function submitClose(callback: SubmitCallback<'close'>): RequestHandler {
+  return async function (req, res) {
+    let message: Message<MessageKind>
+
+    try {
+      message = await Message.parse(req.body)
+    } catch(e) {
+      const errorResponse: ErrorDetail = { detail: e.message }
+      return res.status(400).json({ errors: [errorResponse] })
+    }
+
+    if (!message.isClose()) {
+      const errorResponse: ErrorDetail = { detail: 'expected request body to be a valid rfq' }
+      return res.status(400).json({ errors: [errorResponse] })
+    }
+
+    // TODO: get most recent message added to exchange. use that to see if close is allowed
+    // TODO: return 404 if exchange not found
+    // TODO: return 409 if close is not allowed given the current state of the exchange
+
+    if (!callback) {
+      // TODO: figure out what to do
+      return res.sendStatus(202)
+    }
+
+    let result;
+    try {
+      result = await callback({ request: req, response: res }, message)
+    } catch(e) {
+      // TODO: handle error lewl
+    }
+
+    return res.sendStatus(501)
+  }
 }
