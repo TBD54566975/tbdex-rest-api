3c80aec0eb9aa1bb9a192b51a8c2181b2311fdf3
Moe Jangda <moe@tbd.email>
2023-09-05 22:22:52 -0500

    scaffold `getOfferings`

--- src/request-handlers/get-offerings.ts
+++ src/request-handlers/get-offerings.ts
@@ -1,5 +1,7 @@
 import type { GetCallback, RequestHandler } from '../types.js'
 
 export function getOfferings(_callback: GetCallback<'offerings'>): RequestHandler {
-  return null
+  return async function (req, res) {
+    return res.sendStatus(501)
+  }
 }
